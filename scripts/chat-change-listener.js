183a6cf1343fc337654105cca284a64a8d9790ed
Dylan Baumann <dylanbaumanndesign@gmail.com>
2018-11-01 22:43:42 -0700

    Renames extension, reduces size of Skill Messages, and auto-scrolls chat when new messages appear to avoid half-messages from appearing

--- scripts/chat-change-listener.js
+++ scripts/chat-change-listener.js
@@ -11,9 +11,28 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 // ------------------------------------------------------------------------
 
+// Whenever chat updates, scroll the chat box down to display the new message
+// since that can occasionaly get bugged and clip messages or only show half-messages
+const scrollChatDown = () => {
+	let chatContainer = document.querySelector('.message-container');
+
+	chatContainer.scrollTop = chatContainer.scrollHeight;
+
+	console.log('chat has been scrolled down automatically')
+}
+
 // Creates a callback function to execute whenever a DOM Mutation is observed
 const chatChangeCallback = function(chatChangeList, chatObserver) {
-	for (let mutation of chatChangeList) {
+
+	// Fire off the ScrollChatDown function
+	scrollChatDown();
+
+	/*  ---------------------------------------------
+		The following section is for console logging
+		the specific events which can happen inside of
+    	chat. I'm removing it for now.
+    	  ---------------------------------------- */
+	/* for (let mutation of chatChangeList) {
 		// If an item was added/removed
 		if (mutation.type == 'childList') {
 			console.log('A child node has been added or removed.');
@@ -24,13 +43,14 @@ const chatChangeCallback = function(chatChangeList, chatObserver) {
 			console.log('The ' + mutation.attributeName + ' attribute was modified.');
 		}
 	}
+	*/
 };
 
-// So browser extensions run code as soon as it's available, which is great, but
-// this can be a problem since Mixer loads elements dynamically after the server
-// response is recieved from the browser. Long story short, this means that the
-// chat box isn't always available right away. This function checks to see if
-// chat exists, then re-checks every 0.5s until it does.
+/** So browser extensions run code as soon as it's available, which is great, but
+	this can be a problem since Mixer loads elements dynamically after the server
+	response is recieved from the browser. Long story short, this means that the
+	chat box isn't always available right away. This function checks to see if
+	chat exists, then re-checks every 0.5s until it does. */
 function addObserverIfChatExists() {
 	// Attempt to find the chat box
 	let chatContainer = document.querySelector('.message-container');
